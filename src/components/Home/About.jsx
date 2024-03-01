69b742005d7341894f73830a77f0bffa4ef67b8d
vlady <vlady3000hc@gmail.com>
2024-03-01 14:54:13 -0400

    three port

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -32,7 +32,7 @@ export const About = () => {
             <div className="flex justify-center w-full mt-8">
                 <a
                     className="flex items-center gap-2 px-6 py-2 font-bold relative z-10 overflow-hidden before:content-[''] before:absolute before:top-0 before:left-0 before:h-full before:w-0 before:rounded-full before:bg-yellow-600 before:-z-10 before:transition-all before:duration-300 before:hover:w-full text-dark-100 bg-dark-800 rounded-full hover:bg-dark-600 hover:shadow-md hover:shadow-yellow-800/50 ease duration-500"
-                    href="https://docs.google.com/document/d/1qroZkOm9Bxvuf5LKPC12N-yTHao0oP0hR7HS_8qyZas/edit?usp=sharing"
+                    href="https://drive.google.com/file/d/1EaoiNNjtdQhyd25fbreU19XvjmyOmmpc/view"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
